4ecacd23df0e11b299f8673b89dbd1fd0751c1fe
prusakovann <56076223+prusakovann@users.noreply.github.com>
2019-11-11 17:31:09 +0500

    Add files via upload

---
+++ ex.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main()
+{
+ int a=3;
+int b;
+    int c = 0; 
+while(scanf("%d" , &b))
+{
+ 
+    if (a==b)
+    {
+        c+=1;
+    }
+    printf("%d\n", c);    
+}     
+}
